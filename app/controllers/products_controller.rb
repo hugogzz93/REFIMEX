4c3aae57fd743a4cddaff6554ed8a8e216b22911
Hugo Gonzalez <pinelo93@gmail.com>
2017-02-25 17:10:06 -0600

    Adds nested attributes for products, fixes null object errors.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,9 +7,19 @@ class ProductsController < CrudController
 
   def edit
     super
-    @product_prices = @object.product_prices.order(active_date: :desc)
     @users = User.all
-    gon.clear
-    gon.chart_data = ProductPrice.chart_digest({product_id: @object.id}, Time.zone.now, "month")
+    @product_prices = @object.product_prices.order(active_date: :desc)
+    gon.chart_data = ProductPrice.chart_digest({product_id: @object.id},
+                                               Time.zone.now, "month")
+  end
+
+  private
+
+  def object_params
+    super.merge params.require(:product)
+                      .permit(product_prices_attributes: [:fiscal_stimulus,
+                                               :diminished_quote,
+                                               :final_price,
+                                               :active_date])
   end
 end
