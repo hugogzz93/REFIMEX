3d682a171b89033da6425889175deae861542255
Hugo Gonzalez <pinelo93@gmail.com>
2017-04-02 17:29:16 -0500

    WIP frontpage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,7 @@
 class UsersController < CrudController
+	before_action :check_if_allowed
+
+	def check_if_allowed
+		permit_if_admin unless ["edit", "update"].include?(params[:action]) && params[:id].to_i == current_user.id
+	end
 end
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,17 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( frontpage.css )
 Rails.application.config.assets.precompile += %w( frontpage.coffee )
+
+Rails.application.config.assets.precompile += %w( img/thumb/fuel_station )
+Rails.application.config.assets.precompile += %w( img/thumb/refmex_logo )
+Rails.application.config.assets.precompile += %w( img/thumb/tubes )
+Rails.application.config.assets.precompile += %w( img/thumb/fuel_truck )
+Rails.application.config.assets.precompile += %w( img/thumb/grass )
+Rails.application.config.assets.precompile += %w( img/thumb/refinery )
+Rails.application.config.assets.precompile += %w( img/thumb/fuel )
+Rails.application.config.assets.precompile += %w( img/thumb/trucks )
+Rails.application.config.assets.precompile += %w( img/thumb/general )
+Rails.application.config.assets.precompile += %w( img/thumb/blue_truck )
+Rails.application.config.assets.precompile += %w( img/thumb/fueling )
+
+
