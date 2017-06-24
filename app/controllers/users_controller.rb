49a0f5407b3c62a7e448ab9bdd7b05961e64c3c4
Hugo Gonzalez <pinelo93@gmail.com>
2017-06-24 15:26:08 -0500

    Fixes issue with password changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,11 @@ class UsersController < CrudController
 	def check_if_allowed
 		permit_if_admin unless ["edit", "update"].include?(params[:action]) && params[:id].to_i == current_user.id
 	end
+
+		def object_params
+			super.merge params.require(:user)
+												.permit(:password, :password_confirmation)
+	
+		end
+
 end
